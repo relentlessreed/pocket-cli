1292d6055c0db2db211fea03a7f5f8c5c26d350d
Edward Reed <relentlessreed@gmail.com>
2023-03-19 02:09:24 -0500

    HUGE commit, the index.html is significantly smaller now AND the loading bar is working, almost!

--- public/main.js
+++ public/main.js
@@ -4,179 +4,61 @@ document.addEventListener("DOMContentLoaded", () => {
   const flashingText = document.getElementById("flashing-text");
   const nowLoadingText = document.getElementById("now-loading-text");
   const filler = document.getElementById("filler");
-  let progress = 0;
+  let loading = 0;
   let loadingFinished = false;
-  //   const barEnd = document.querySelector(".bar-end");
-  //   const progressbar = document.getElementById("progress-bar");
-  //   const charactersToLoad = 74;
-  //   let charactersLoaded = 0;
 
   console.log("Script loaded");
 
-  //   function updateLoadingBar() {
-  //     charactersLoaded += 1;
-
   function updateLoadingBar() {
-    // Occasionally make the loading bar stutter
     if (Math.random() < 0.1) {
       setTimeout(() => {
-        progress += Math.random() * 5;
-        updateProgressBar();
+        loading += Math.random() * 5;
+        updateLoadingBar();
       }, 100 + Math.random() * 300);
     } else {
-      progress += Math.random() * 3;
-      updateProgressBar();
+      loading += Math.random() * 3;
+      updateLoadingBar();
     }
 
-    // function to show the Terminal after splash screen is clicked
-    function showTerminal() {
-      terminal.style.display = "block";
-      terminal.textContent = "Type your command here";
-      terminal.classList.add("flashing-text");
-    }
-
-    // Function to hide the splash screen
-    const hideSplashScreen = () => {
-      splashScreen.style.display = "none";
-      splashScreen.removeEventListener("click", hideSplashScreen);
-      setTimeout(showTerminal, 500); // Adjust the timeout value as needed
-      terminal.focus();
-    };
-
-    // Hide the splash screen when clicked
-    splashScreen.addEventListener("click", hideSplashScreen);
-
-    terminal.addEventListener("keydown", (e) => {
-      // Remove initial text when the user starts typing
-      if (!terminal.classList.contains("user-started-typing")) {
-        terminal.textContent = "";
-        terminal.classList.add("user-started-typing");
-      }
-
-      // Check for Ctrl + C key combination
-      if (e.ctrlKey && e.key === "c") {
-        e.preventDefault();
-        e.stopPropagation();
-        terminal.textContent = "";
-        // terminal.focus();
-        // Move caret to the beginning
-        const range = document.createRange();
-        const selection = window.getSelection();
-        range.setStart(terminal.firstChild, 0);
-        range.collapse(true);
-        selection.removeAllRanges();
-        selection.addRange(range);
-        return;
-      }
-
-      if (e.key === "Enter") {
-        e.preventDefault();
-        const input = terminal.textContent.trim().split("\n").pop();
-        processCommand(input);
-      }
-    });
-
-    //   function updateLoadingBar() {
-    //     progress += 1;
-
-    //   function updateProgressBar() {
-    //     charactersLoaded += 1;
-
-    // Calculate the percentage of characters loaded
-    const percentageLoaded = (charactersLoaded / charactersToLoad) * 100;
-
-    if ((progress += 1)) {
+    if (loading >= 100) {
       clearInterval(loadingInterval);
-      flashingText.classList.remove("hidden"); // Show the flashing text
-      nowLoadingText.classList.add("hidden"); // Hide the "Now Loading:" text
-      loadingFinished = true; // Set loadingFinished to true
-      splashScreen.addEventListener("click", hideSplashScreen); // Listen for click event to hide splash screen
+      flashingText.classList.remove("hidden");
+      nowLoadingText.classList.add("hidden");
+      loadingFinished = true;
+      splashScreen.addEventListener("click", hideSplashScreen);
       return;
     }
 
-    const barWidth = Math.max(progress * 1); // Adjust this value to change the number of '*' characters.
-    filler.textContent = "*".repeat(72);
-    barEnd.style.marginLeft = `${100 - progress}%`;
+    filler.textContent = "|".repeat(Math.floor(loading));
   }
 
-  const loadingInterval = setInterval(updateLoadingBar, 100); // Adjust the 100 value to change the speed of the loading bar.
+  const loadingInterval = setInterval(updateLoadingBar, 100);
 
-  // Set focus to the terminal after hiding the splash screen
-  splashScreen.addEventListener("transitionend", () => {
-    // terminal.focus();
-    // setTimeout(() => {
-    //   splashScreen.style.display = "none";
-    // }, 8500);
-  });
-});
-
-terminal.addEventListener("mousedown", (e) => {
-  const terminalRect = terminal.getBoundingClientRect();
-  const clickPosition = e.clientX - terminalRect.left;
-
-  if (clickPosition > terminalRect.width / 2) {
-    setCaretToTopLeft();
-  }
-});
-
-terminal.addEventListener("keydown", (e) => {
-  // Check for Ctrl + L key combination
-  if (e.ctrlKey && e.key === "l") {
-    e.preventDefault();
-    e.stopPropagation(); // Add this line
-    terminal.textContent = "";
-    // terminal.focus();
-    // Move caret to the beginning
-    const range = document.createRange();
-    const selection = window.getSelection();
-    range.setStart(terminal.firstChild, 0);
-    range.collapse(true);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    return;
+  function showTerminal() {
+    terminal.style.display = "block";
+    terminal.textContent = "Type your command here";
+    terminal.classList.add("flashing-text");
   }
 
-  if (e.key === "Enter") {
-    e.preventDefault();
-    const input = terminal.textContent.trim().split("\n").pop();
-    processCommand(input);
-  }
-});
+  const hideSplashScreen = () => {
+    splashScreen.style.display = "none";
+    splashScreen.removeEventListener("click", hideSplashScreen);
+    setTimeout(showTerminal, 500);
+    terminal.focus();
+  };
 
-let currentDirectory = "/";
+  splashScreen.addEventListener("click", hideSplashScreen);
 
-function setCaretToTopLeft() {
-  const range = document.createRange();
-  const selection = window.getSelection();
-  range.setStart(terminal.firstChild, 0);
-  range.collapse(true);
-  selection.removeAllRanges();
-  selection.addRange(range);
-}
-
-function processCommand(input) {
-  // ...
-  // (The rest of the processCommand function)
-  // ...
-
-  terminal.textContent += `\n${output}\n`;
-  preserveCaretPosition();
-}
-
-function processCommand(input) {
-  const command = input.split(" ")[0];
-  let output = "";
-
-  switch (command) {
-    case "help":
-      output = "Available commands: help, echo, clear, pwd, ls, cd";
-      break;
-    case "echo":
-      output = input.substring(input.indexOf(" ") + 1);
-      break;
+  terminal.addEventListener("keydown", (e) => {
+    if (!terminal.classList.contains("user-started-typing")) {
       terminal.textContent = "";
-      //   terminal.focus();
-      // Move caret to the beginning
+      terminal.classList.add("user-started-typing");
+    }
+
+    if (e.ctrlKey && e.key === "c") {
+      e.preventDefault();
+      e.stopPropagation();
+      terminal.textContent = "";
       const range = document.createRange();
       const selection = window.getSelection();
       range.setStart(terminal.firstChild, 0);
@@ -184,50 +66,16 @@ function processCommand(input) {
       selection.removeAllRanges();
       selection.addRange(range);
       return;
-      return;
-    case "pwd":
-      output = currentDirectory;
-      break;
-    case "ls":
-      output = Object.keys(fileSystem[currentDirectory]).join(" ");
-      break;
-    case "cd":
-      const targetDirectory = input.split(" ")[1];
-      if (fileSystem.hasOwnProperty(targetDirectory)) {
-        currentDirectory = targetDirectory;
-      } else {
-        output = `cd: ${targetDirectory}: No such directory`;
-      }
-      break;
-    default:
-      output = `Unknown command: ${command}`;
+    }
+
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const input = terminal.textContent.trim().split("\n").pop();
+      processCommand(input);
+    }
+  });
+
+  function processCommand(input) {
+    // Add the rest of the processCommand function and other event listeners here
   }
-
-  terminal.textContent += `\n${output}\n`;
-  //   terminal.focus();
-}
-
-// terminal.focus();
-
-// Other functions and event listeners
-// script.js
-const filler = document.querySelector(".filler");
-let progress = 0;
-
-function updateLoadingBar() {
-  progress += 1;
-
-  if (progress > 100) {
-    clearInterval(loadingInterval);
-    return;
-  }
-
-  //   // Update the loading bar width
-  //   loadingBar.style.width = `${percentageLoaded}%`;
-
-  //   const barWidth = Math.floor(progress * 1); // Adjust this value to change the number of '*' characters.
-  //   filler.textContent = "*".repeat(barWidth);
-  //   filler.style.width = `${progress}%`;
-}
-
-const loadingInterval = setInterval(updateLoadingBar, 100); // Adjust the 100 value to change the speed of the loading bar.
+});
