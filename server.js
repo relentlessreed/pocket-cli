c8fffacea1171ea464c6d7e6c0efdc15664c7cb0
Edward Reed <relentlessreed@gmail.com>
2023-03-19 23:09:19 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const version = process.env.VERSION;
-console.log("Version:", packageJson.version);
+console.log("Version:", version);
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -13,18 +13,10 @@ app.get("/", (req, res) => {
 });
 
 const packageJsonPath = path.join(__dirname, "package.json");
-let packageJson;
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
 
-fs.readFile(packageJsonPath, "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading package.json:", err);
-    return;
-  }
-  packageJson = JSON.parse(data);
-
-  app.get("/version", (req, res) => {
-    res.json({ version: packageJson.version });
-  });
+app.get("/version", (req, res) => {
+  res.json({ version: packageJson.version });
 });
 
 app.get("/increment-version", (req, res) => {
@@ -37,16 +29,10 @@ app.get("/increment-version", (req, res) => {
 
   packageJson.version = updatedVersion;
 
-  fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
-    if (err) {
-      console.error("Error updating package.json:", err);
-      res.status(500).send("Error updating package.json");
-      return;
-    }
-    console.log("Released v" + updatedVersion + " (updatedVersion)");
-    console.log("https://pocket-cli.herokuapp.com deployed to Heroku");
-    res.send("Version incremented successfully");
-  });
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  console.log("Released v" + updatedVersion + " (updatedVersion)");
+  console.log("https://pocket-cli.herokuapp.com deployed to Heroku");
+  res.send("Version incremented successfully");
 });
 
 const PORT = process.env.PORT || 3000;
