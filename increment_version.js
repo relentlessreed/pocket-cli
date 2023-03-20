5dede7e5eb1d7e1e39ea22197de72933945af02b
Edward Reed <relentlessreed@gmail.com>
2023-03-19 19:49:16 -0500

    Add postinstall script to increment version

--- increment_version.js
+++ increment_version.js
@@ -1,20 +1,33 @@
-const fs = require('fs');
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 
-const packageJsonPath = path.join(__dirname, 'package.json');
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const packageJsonPath = path.join(__dirname, "package.json");
 
-const currentVersion = packageJson.version.split('.');
-const updatedVersion = [
-  currentVersion[0],
-  currentVersion[1],
-  parseInt(currentVersion[2]) + 1,
-].join('.');
+fs.readFile(packageJsonPath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading package.json:", err);
+    process.exit(1);
+  }
 
-packageJson.version = updatedVersion;
+  const packageJson = JSON.parse(data);
+  const currentVersion = packageJson.version.split(".");
+  const updatedVersion = [
+    currentVersion[0],
+    currentVersion[1],
+    parseInt(currentVersion[2]) + 1,
+  ].join(".");
 
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-console.log('Updated package.json version to', updatedVersion);
+  packageJson.version = updatedVersion;
+
+  fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
+    if (err) {
+      console.error("Error updating package.json:", err);
+      process.exit(1);
+    }
+
+    console.log("Updated version:", updatedVersion);
+  });
+});
 
 // OLD "SOLUTION" (build_number.js)
 
